9c60b022bb03f7900f63492c90b6048ec7efc601
sherryyu <bambooyu715@gmail.com>
2023-05-25 10:59:32 -0400

    finished inital set up, ready to build models

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,35 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy import func
 
+follows = db.Table(
+    "follows", 
+    db.Column("follower_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("followed_id", db.Integer, db.ForeignKey("users.id"))
+)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    profile_url = db.Column(db.Text)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
+    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+
+    followers = db.relationship(
+        "User", 
+        secondary=follows,
+        primaryjoin=(follows.c.follower_id == id),
+        secondaryjoin=(follows.c.followed_id == id),
+        backref=db.backref("following", lazy="dynamic"),
+        lazy="dynamic"
+    )
 
     @property
     def password(self):
@@ -21,10 +41,19 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    # def follow(self, user):
+    #     if user.id not in self.followers:
+    #         self.follo
 
     def to_dict(self):
         return {
             'id': self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            "profile_url": self.profile_url,
+            "followers": self.followers,
+            "follows": self.following
         }
