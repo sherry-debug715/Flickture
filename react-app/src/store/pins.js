138705ab5b8669d0223de903f8fd6a49cfaa87ea
sherryyu <bambooyu715@gmail.com>
2023-05-30 13:16:18 -0400

    build get all pins thunk

--- react-app/src/store/pins.js
+++ react-app/src/store/pins.js
@@ -1,3 +1,35 @@
+GET_ALL_PINS = "pins/GET_ALL_PINS"
 
+const getAllPins = pins => ({
+    type: GET_ALL_PINS,
+    pins
+});
 
-// {}
+export const getAllPinsThunk = (page) => async dispatch => {
+    const response = await fetch(`/api/pins?page=${page}`, {
+        headers: {"Content-Type": "application/json"}
+    });
+
+    if(response.ok){
+        const allPins = await response.json();
+        dispatch(getAllPins(allPins.pins));
+    };
+};
+
+const normalization = (arr) => {
+    const normalized = {};
+    arr.forEach(obj => normalized[obj.id] = obj);
+    return normalized;
+};
+
+const initialState = {allPins:{}, singlePin:{}};
+
+export default function pinReducer(state = initialState, action) {
+    let newState;
+    switch(action.type) {
+        case GET_ALL_PINS:
+            newState = {...state};
+            newState.allPins = {...newState.allPins,...normalization(action.pins)};
+            return newState;
+    };
+};
