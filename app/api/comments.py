c0de2f5b291356fcc44754549df5529f48071349
sherryyu <bambooyu715@gmail.com>
2023-07-02 15:40:20 -0400

    build edit comment route

--- app/api/comments.py
+++ app/api/comments.py
@@ -4,6 +4,38 @@ from app.models import db, Pin, Comment, User
 
 comment_routes = Blueprint("comments", __name__)
 
+@comment_routes.route('/edit/<int:pin_id>/<int:comment_id>', methods=["PUT"])
+@login_required
+def edit_comment(pin_id, comment_id):
+    find_pin = Pin.query.get(pin_id)
+
+    if find_pin is None:
+        return jsonify({"error": "Pin not found"}), 404
+    
+    data = request.get_json()
+
+    content = data["content"]
+
+    edited_comment = Comment.query.get(comment_id)
+
+    if edited_comment is None:
+        return {"Error": f"Comment with id {comment_id} not found"}, 404
+    
+    user_id = current_user.id 
+
+    edit_comment_user_id = edited_comment.user_id
+
+    if user_id != edit_comment_user_id:
+        return {"Error": "Only creator of the comment has acceess to edit the comment"}
+    
+    edited_comment.content = content
+
+    db.session.add(edited_comment)
+    db.session.commit()
+
+    return jsonify(edited_comment.to_dict()), 200
+  
+
 @comment_routes.route('/create/<int:pin_id>', methods=["POST"])
 @login_required
 def create_comment(pin_id):
