5a6203c0721332ae6e4a874ee12b5d93e2ea550a
sherryyu <bambooyu715@gmail.com>
2023-07-06 10:02:24 -0400

    trying again

--- react-app/src/store/pins.js
+++ react-app/src/store/pins.js
@@ -171,7 +171,7 @@ export const getAllPinsThunk = (page, searchQuery="") => async dispatch => {
 
     let url;
     if(process.env.NODE_ENV === "production") {
-        url = `https://flickture-b5784f016188.herokuapp.com/api/pins?page=${page}&search=${encodeURIComponent(searchQuery)}`
+        url = `https://flickture-b5784f016188.herokuapp.com/api/pins?page=${page}&search=${searchQuery}`
     } else {
         url = `/api/pins?page=${page}&search=${encodeURIComponent(searchQuery)}`
     };
