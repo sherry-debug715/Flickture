b6972250cb217ed15656d97781c263aa58485fcd
sherryyu <bambooyu715@gmail.com>
2023-11-08 16:09:56 -0500

    optimized search feature so it displays messages when user hit the bottom of the page or when there is nothing match the search

--- react-app/src/components/pins/getPins/GetAllPins.js
+++ react-app/src/components/pins/getPins/GetAllPins.js
@@ -8,6 +8,7 @@ import PinCard from "../PinCard";
 export default function GetAllPins({searchContent}) {
     const dispatch = useDispatch();
     const allPins = useSelector(state => state.pins.allPins);
+    console.log("allPins", allPins)
     const savedPinsState = useSelector(state => state.pins.savedPins);
     const sessionUser = useSelector(state => state.session.user);
     const pinArr = Object.values(allPins.pins);
@@ -15,6 +16,7 @@ export default function GetAllPins({searchContent}) {
 
     const [page, setPage] = useState(1);
     const [readyToFetch, setReadyToFetch] = useState(false);
+    const [hitBottom, setHitBottom] = useState(false);
 
     const scrollContainerRef = useRef();
 
@@ -33,7 +35,11 @@ export default function GetAllPins({searchContent}) {
 
         const calculatingScroll = scrollContainer.scrollHeight - scrollContainer.scrollTop - scrollContainer.clientHeight;
 
-        if (calculatingScroll > 1 || page === totalPage) return;        
+        if (calculatingScroll > 1) return;  
+        if (page === totalPage) {
+            setHitBottom(true);
+            return;
+        }   
         else setPage(prev => prev + 1);
     }, [readyToFetch, page, totalPage]);
 
@@ -60,16 +66,27 @@ export default function GetAllPins({searchContent}) {
         }
     }, [dispatch, page]);
 
-
-
     return (
-        <div className="all-pins-container" ref={scrollContainerRef}>
-            <div className="all-pins-image-container"  >
-                {pinArr.map(pin => (
-                    <PinCard pin={pin} key={pin.id} pinSaved={pinSaved} />            
-                ))}
+        pinArr.length > 0 ? (
+            <>
+                <div className="all-pins-container" ref={scrollContainerRef}>
+                    <div className="all-pins-image-container">
+                        {pinArr.map(pin => (
+                            <PinCard pin={pin} key={pin.id} pinSaved={pinSaved} />
+                        ))}
+                    </div>
+                </div>
+                {hitBottom &&
+                <div
+                    className="all-pins-end-of-content-message"
+                >Looks like you've reached the end, check back soon for updates.</div>}
+            </>
+        ) : (
+            <div
+                className="all-pins-no-search-results-message"
+            >
+                We couldn't find anything matching your search. Try different keywords!
             </div>
-        </div>
+        )
     );
-
 };
